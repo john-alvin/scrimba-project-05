609fe0e14a032f2f806b24e812b8852ff8f59e25
John Alvin <johnalvin.bonifacio@proton.me>
2022-11-21 08:41:29 +0800

    create function to access random url in the memesData

--- src/component/Meme.js
+++ src/component/Meme.js
@@ -2,11 +2,20 @@ import React from "react";
 import memeData from "../memeData";
 
 function Meme() {
+  let url;
+  function getImage() {
+    const memesArray = memeData.data.memes;
+    const randomNumbers = Math.floor(Math.random() * memesArray.length);
+
+    url = memesArray[randomNumbers].url;
+  }
   return (
     <form className="form">
       <input className="input-text" placeholder="input text here" type="text" />
       <input className="input-text" placeholder="input text here" type="text" />
-      <button className="button-generator">get a new meme image</button>
+      <button className="button-generator" onClick={getImage}>
+        get a new meme image
+      </button>
     </form>
   );
 }
